722ee7bb54c35505b6326f7720b34891c239f7a7
Josué Reyes <josue.hreyess@gmail.com>
2022-04-22 09:36:39 -0500

    Add Splashscreen

--- app/src/main/java/com/example/quoteit/activities/SignIn.kt
+++ app/src/main/java/com/example/quoteit/activities/SignIn.kt
@@ -3,6 +3,7 @@ package com.example.quoteit.activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import com.example.quoteit.R
@@ -17,6 +18,7 @@ class SignIn : AppCompatActivity(), SignInView {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContentView(R.layout.activity_sign_in)
 
         val navHostFragment = supportFragmentManager
